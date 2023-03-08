2a7e8519f3dd06f23f5a8880b60ba0d303445b89
maxime_dutres <maxime.dutres@mydid.com>
2023-03-08 10:39:13 +0100

    Authorize VP signed by issuer.

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,35 @@ const mydidAuth = {
     return new VerifiablePresentationRequest(challenge, domain, verifiableCredentials);
   },
 
-  validateVPConsistency: (VPData: object): boolean => {
+  validateVPConsistency: (VPData: object): object => {
     if (!isVerifiablePresentationSchema(VPData)) throw 'Incorrect format for verifiable presentation';
 
     const verifiablePresentation: VerifiablePresentation = VPData as VerifiablePresentation;
 
+    let vpSignerIsSender = true;
+    let vpSignerIsReceiver = true;
+
     if (verifiablePresentation.verifiableCredential) {
       for (const verifiableCredential of verifiablePresentation.verifiableCredential) {
-        if (verifiableCredential.credentialSubject.id != verifiablePresentation.id)
-          throw 'Incorrect id in credential subjects';
+        if (verifiableCredential.credentialSubject.id != verifiablePresentation.id) vpSignerIsReceiver = false;
+        if (
+          verifiableCredential.proof.verificationMethod.split('#')[0] !=
+          verifiablePresentation.proof.verificationMethod.split('#')[0]
+        )
+          vpSignerIsSender = false;
       }
     }
 
-    return true;
+    if (!vpSignerIsReceiver && !vpSignerIsSender) throw `Incorrect signer for verifiable presentation`;
+
+    return {
+      status: 'validated',
+      vpSignerIsReceiver,
+      vpSignerIsSender,
+    };
   },
 
-  validateVPAuthenticity: async (VPData: object): Promise<boolean> => {
+  validateVPAuthenticity: async (VPData: object): Promise<object> => {
     var verifiablePresentation: VerifiablePresentation = VPData as VerifiablePresentation;
 
     await Promise.all([
@@ -38,7 +51,9 @@ const mydidAuth = {
         : []),
     ]);
 
-    return true;
+    return {
+      status: 'validated',
+    };
   },
 };
 
